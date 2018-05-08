b890bcf11e7d1dc32e6c054c7b732e2b4628dad6
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-05-08 16:22:49 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -21,6 +21,6 @@ function destructivelyAddElementToEndOfArray(array, element) {
   return array
 }
 
-function accessAnyElement(array, index) {
+function accessElementInArray(array, index) {
   return array[ index ];
 }
