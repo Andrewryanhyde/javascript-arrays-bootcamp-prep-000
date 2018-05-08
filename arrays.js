234f61d590f0a1a86e96efd7a46d7049fe3ac6e0
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-05-08 14:27:11 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,10 @@
+let chocolateBars = [snickers, hundred grand, kitkat, skittles]
+
+function addElementToBeginningOfArray(array, element) {
+  
+}
+
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  
+}
