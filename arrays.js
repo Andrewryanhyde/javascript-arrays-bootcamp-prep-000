a7e5d9da5c61ff3cbb5f403f8eb7723ee192a4d6
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-05-08 16:05:27 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -12,7 +12,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 function addElementToEndOfArray(array, element) {
-  var newArray = array.pop(element) 
+  var newArray = element 
   return newArray;
 }
 
